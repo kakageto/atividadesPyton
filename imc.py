26c0d3877ad82eee449710a52bcac4793323b354
kakageto <karineveloso008@gmail.com>
2025-08-07 14:49:12 +0000

    salvar

--- imc.py
+++ imc.py
@@ -2,13 +2,13 @@ def classificacao_imc(peso, altura):
     imc = peso / (altura ** 2)
     print(f"IMC calculado: {imc:.2f}")
     
-    if imc < 18.5:
+    if imc <=18.5 and imc<=24.9:
         print("Classificação: Abaixo do peso")
-    elif imc < 25:
+    elif imc <=25 and imc <=29.9:
         print("Classificação: Peso normal")
-    elif imc < 30:
+    elif imc ==30:
         print("Classificação: Sobrepeso")
-    else:
+    elif imc >40:
         print("Classificação: Obesidade")
 
 classificacao_imc(60, 1.60)
